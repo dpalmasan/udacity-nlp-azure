47f129851d4b320c4b4414ad414db1a884786e01
dpalmasan <dpalmasan@gmail.com>
2022-05-08 21:21:10 -0700

    Add template .env file

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -8,52 +8,52 @@ const DentistScheduler = require('./dentistscheduler');
 const IntentRecognizer = require("./intentrecognizer")
 
 class DentaBot extends ActivityHandler {
-    constructor(configuration, qnaOptions) {
-        // call the parent constructor
-        super();
-        if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
+  constructor(configuration, qnaOptions) {
+    // call the parent constructor
+    super();
+    if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
-        // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
-       
-        // create a DentistScheduler connector
-      
-        // create a IntentRecognizer connector
+    // create a QnAMaker connector
+    this.QnAMaker = new QnAMaker(configuration.QnAConfiguration)
+
+    // create a DentistScheduler connector
+
+    // create a IntentRecognizer connector
 
 
-        this.onMessage(async (context, next) => {
-            // send user input to QnA Maker and collect the response in a variable
-            // don't forget to use the 'await' keyword
-          
-            // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
-                     
-            // determine which service to respond with based on the results from LUIS //
+    this.onMessage(async (context, next) => {
+      // send user input to QnA Maker and collect the response in a variable
+      // don't forget to use the 'await' keyword
 
-            // if(top intent is intentA and confidence greater than 50){
-            //  doSomething();
-            //  await context.sendActivity();
-            //  await next();
-            //  return;
-            // }
-            // else {...}
-             
-            await next();
+      // send user input to IntentRecognizer and collect the response in a variable
+      // don't forget 'await'
+
+      // determine which service to respond with based on the results from LUIS //
+
+      // if(top intent is intentA and confidence greater than 50){
+      //  doSomething();
+      //  await context.sendActivity();
+      //  await next();
+      //  return;
+      // }
+      // else {...}
+
+      await next();
     });
 
-        this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        //write a custom greeting
-        const welcomeText = '';
-        for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-            if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-            }
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      //write a custom greeting
+      const welcomeText = '';
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
         }
-        // by calling next() you ensure that the next BotHandler is run.
-        await next();
+      }
+      // by calling next() you ensure that the next BotHandler is run.
+      await next();
     });
-    }
+  }
 }
 
 module.exports.DentaBot = DentaBot;
